15846cb030627aabb02937aa9228083c21bec8e8
Takashi Yamamiya <tyamamiya@xevo.com>
2017-08-09 08:24:39 +0000

    Add Qt::FramelessWindowHint to show from AGL HomeScreen

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QLabel label("Hello, World!");
+    label.setWindowFlags(Qt::FramelessWindowHint);
     label.show();
     return a.exec();
 }
